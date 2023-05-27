00f2e0e5c88fc5a4cc1066cc03209ab80aebed06
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-27 23:49:57 +0200

    translation endpoint added

---
+++ app/GraphQL/Queries/Translate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Services\TranslationService;
+
+final class Translate
+{
+    private TranslationService $translationService;
+
+    public function __construct(TranslationService $translationService)
+    {
+        $this->translationService = $translationService;
+    }
+    public function translate($root, array $args)
+    {
+        $originalLanguage = $args['original_language'];
+        $targetLanguage = $args['target_language'];
+        $textType = $args['text_type'];
+        $text = $args['text'];
+
+        return $this->translationService->translate($originalLanguage, $targetLanguage, $textType, $text);
+    }
+}
