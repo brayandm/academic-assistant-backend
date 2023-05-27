ed4309eb9f055417a1659435e8acf40cefe24c20
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-05-28 01:14:44 +0200

    engine task model added

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    public function engineTasks()
+    {
+        return $this->hasMany(EngineTask::class);
+    }
 }
