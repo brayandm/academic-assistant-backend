3fe3cfdd14f9908c0093cfeadd11a6fa7626adfe
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-19 00:37:36 +0200

    grapqh add to test cases

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,18 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseMigrations, RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
     /**
      * A basic test example.
      */
