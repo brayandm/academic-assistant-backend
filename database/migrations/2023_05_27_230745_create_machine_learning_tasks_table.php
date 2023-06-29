830ddcb8f98708d9012d97cc00f90b3013f8b785
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-29 01:13:03 +0000

    updating names

--- database/migrations/2023_05_27_230745_create_machine_learning_tasks_table.php
+++ database/migrations/2023_05_27_230745_create_machine_learning_tasks_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('engine_tasks', function (Blueprint $table) {
+        Schema::create('machine_learning_tasks', function (Blueprint $table) {
             $table->id();
             $table->string('task_id');
             $table->string('task_type');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('engine_tasks');
+        Schema::dropIfExists('machine_learning_tasks');
     }
 };
