73fc3f5f741982f5eb4dda970f266528039891f5
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-11 02:02:08 +0200

    quotas added

--- app/Http/Middleware/EngineUsage.php
+++ app/Http/Middleware/EngineUsage.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Models\AiModel;
 use App\Models\MachineLearningTask;
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,6 +32,14 @@ class EngineUsage
             ]);
 
             $machineLearningTask->aiModels()->attach($aiModel->id, ['usage' => $ai_model['usage']]);
+
+            $user = User::find($machineLearningTask->user_id);
+
+            $quota = $user->quotas()->where('ai_model_id', $aiModel->id)->first();
+
+            $user->quotas()->updateExistingPivot($aiModel->id, ['quota' => $quota->pivot->quota - $ai_model['usage']]);
+
+            $quota->save();
         }
 
         return $next($request);
